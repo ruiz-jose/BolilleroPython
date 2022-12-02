47ce52abbdeae49c29910aa4833e8c44c2472cbb
ruiz-jose <108295824+ruiz-jose@users.noreply.github.com>
2022-12-02 19:04:56 -0300

    Update bolillero.py
    dos opciones de bolillero

--- bolillero.py
+++ bolillero.py
@@ -2,5 +2,6 @@
 import random
 
 # sample saca dos bolillas del bolillero
-bolillero = [1, 2, 3, 4, 5, 6]
+#bolillero = [1, 2, 3, 4, 5, 6]
+bolillero = [1, 2, 3, 4, 5, 6, 7, 8]
 print(random.sample(bolillero, 2))
