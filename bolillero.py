920aa9725c08cae7eefa4895d1b10e4834d16647
ruiz-jose <108295824+ruiz-jose@users.noreply.github.com>
2023-03-13 19:43:30 +0000

    cambio en comentarios

--- bolillero.py
+++ bolillero.py
@@ -1,7 +1,8 @@
 # importamos libreria random
 import random
 
-# sample saca dos bolillas del bolillero
-#bolillero = [1, 2, 3, 4, 5, 6]
+# bolillero contiene las unidades de la asignatura 
 bolillero = [1, 2, 3, 4, 5, 6, 7, 8]
+
+# sample saca dos unidades del bolillero
 print(random.sample(bolillero, 2))
